3636e578997ef9ce3f7550afe2ac0a69d391f644
AunikIslam <aunikislam172@gmail.com>
2025-02-10 21:00:59 +0600

    Team Component Created

---
+++ src/app/main/team/team-manage/team-manage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-manage',
+  imports: [],
+  templateUrl: './team-manage.component.html',
+  styleUrl: './team-manage.component.scss'
+})
+export class TeamManageComponent {
+
+}
