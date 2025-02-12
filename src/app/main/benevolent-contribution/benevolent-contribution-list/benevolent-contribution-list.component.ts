5ce65fbb2c6b011047b0b2b7c11de023cb82073b
AunikIslam <aunikislam172@gmail.com>
2025-02-12 14:26:27 +0600

    Contribution component comments added

--- src/app/main/benevolent-contribution/benevolent-contribution-list/benevolent-contribution-list.component.ts
+++ src/app/main/benevolent-contribution/benevolent-contribution-list/benevolent-contribution-list.component.ts
@@ -11,34 +11,40 @@ import { BenevolentContributionManageComponent } from '../benevolent-contributio
   styleUrl: './benevolent-contribution-list.component.css'
 })
 export class BenevolentContributionListComponent {
-  contributions: Contribution[] = [];
-  totalContribution = signal(0);
+  contributions: Contribution[] = []; // used to display benevolent contributions from users
+  totalContribution = signal(0); // used to display total amount of contribution
 
-  @ViewChild('contributionManageContainer', { read: ViewContainerRef }) contributionManageContainer: ViewContainerRef;
+  @ViewChild('contributionManageContainer', { read: ViewContainerRef }) contributionManageContainer: ViewContainerRef; // dynamic component to create contribution
 
+  // injection of the service class
   constructor(private service: BaseService) {
 
   }
 
   ngOnInit(): void {
+    // load contribution list
     this.reloadDataTable();
   }
 
   openContributionAddComponent(): void {
+    // initiate the creation of contribution create component
     const componentRef = this.contributionManageContainer.createComponent(BenevolentContributionManageComponent);
     componentRef.instance.reloadData.subscribe(pData => {
       if(pData) {
         this.reloadDataTable();
       }
+      // clear the component when contribution is created
       this.contributionManageContainer.clear();
     });
   }
 
+  // load contribution list
   reloadDataTable(): void {
     this.contributions = this.service.getContribution();
     this.calculateTotalContribution();
   }
 
+  // calculate total contribution amount
   calculateTotalContribution(): void {
     const contributionAmount = this.contributions.map(pItem => Number(pItem.contributionReason.amount));
     this.totalContribution.set(contributionAmount.reduce((acc, curr) => acc + curr, 0));
