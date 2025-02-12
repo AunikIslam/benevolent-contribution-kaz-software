4df833236f8ebb3fb0f15a0d2144b1b3e3b3343f
AunikIslam <aunikislam172@gmail.com>
2025-02-12 14:59:09 +0600

    Service class comments added

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -4,18 +4,21 @@ import { Member } from "../dto/member";
 import { ContributionReason } from "../dto/contribution-reason";
 import { Contribution } from "../dto/contribution";
 @Injectable({ providedIn: 'root' })
-export class BaseService {
 
+export class BaseService {
+    // get team list from local storage
     getTeams(): Team[] {
         return localStorage.getItem('teams') ? JSON.parse(localStorage.getItem('teams')) : [];
     }
 
+    // create new team
     saveTeam(pTeam: Team): void {
         const teams: Team[] = this.getTeams();
         teams.push(pTeam);
         localStorage.setItem('teams', JSON.stringify(teams));
     }
 
+    // update team lead for a team
     updateTeamLead(pMember: Member): void {
         const teams = this.getTeams();
         for(let i = 0; i < teams.length; i++) {
@@ -29,10 +32,12 @@ export class BaseService {
         localStorage.setItem('teams', JSON.stringify(teams));
     }
 
+    // get member list from local storage
     getMembers(): Member[] {
         return localStorage.getItem('members') ? JSON.parse(localStorage.getItem('members')) : [];
     }
 
+    // add new member and update local storage
     saveMember(pMember: Member): void {
         const members: Member[] = this.getMembers();
         members.push(pMember);
@@ -42,20 +47,24 @@ export class BaseService {
         }
     }
 
+    // get contributions from local storage
     getContribution(): Contribution[] {
         return localStorage.getItem('contributions') ? JSON.parse(localStorage.getItem('contributions')) : [];
     }
 
+    // save contribution and update local storage
     saveContributions(pContribution: Contribution): void {
         const contributions: Contribution[] = this.getContribution();
         contributions.push(pContribution);
         localStorage.setItem('contributions', JSON.stringify(contributions));
     }
 
+    // get contribution reasons from local storage
     getContributionReasons(): ContributionReason[] {
         return localStorage.getItem('contributionReasons') ? JSON.parse(localStorage.getItem('contributionReasons')) : [];
     }
 
+    // save contribution reasons and update local storage
     saveContributionReason(pReason: ContributionReason): void {
         const reasons: ContributionReason[] = this.getContributionReasons();
         reasons.push(pReason);
